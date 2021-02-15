a4a74480b37542494bf5abf8780c13034a436935
ky3he4ik <misha10311@yandex.ru>
2021-02-15 06:39:56 +0300

    Fix error in equation parsing

--- helper_functions.py
+++ helper_functions.py
@@ -5,11 +5,11 @@ ONLY_DIGITS = ''.join(str(i) for i in range(10))
 DICE_NOTATION = ONLY_DIGITS + 'dD%+-*/hHlL '
 
 
-# get number in [1, max_val] using true random
+# get number in [1, max_val] using "true" random
 def get_random_num(max_val: int) -> int:
     bin_len = len(bin(max_val)) - 2
     num_cnt, mask = bin_len // 8, (1 << (bin_len % 8)) - 1
-    for _ in range(1000):  # 1k tries for true cryptographic random
+    for _ in range(1000):  # 1k tries for "true" cryptographic random
         nums = list(os.urandom(num_cnt + 1))
         nums[0] &= mask
         n = 0
@@ -17,6 +17,7 @@ def get_random_num(max_val: int) -> int:
             n = (n << 8) + i
         if n != 0 and n <= max_val:
             return n
+    # fallback - use default python random
     return random.randrange(max_val) + 1
 
 
@@ -58,7 +59,7 @@ def to_int(data, *_, default, max_v, min_v=1):
 # random_generator - function that returns value from 1 to given argument inclusively
 def roll_processing(s, random_generator):
     i = sanity_bound(s, ONLY_DIGITS + 'd% +-*/()')
-    rest, s = s[i:], s[2:i].strip()
+    rest, s = s[i:], s[:i].strip()
     if len(s) == 0:
         s = 'd'
     i, rolls = 0, []
